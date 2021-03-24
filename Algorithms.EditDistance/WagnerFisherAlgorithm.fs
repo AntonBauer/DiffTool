940bd7dd4633b9a9b07b4d32a656b6f701e60b5f
BauerAV <abton.bauer@gmail.com>
2021-03-24 08:09:54 +0100

    pattern matching diff matrix init

--- Algorithms.EditDistance/WagnerFisherAlgorithm.fs
+++ Algorithms.EditDistance/WagnerFisherAlgorithm.fs
@@ -3,18 +3,28 @@ namespace Algorithms.EditDistance
   module WagnerFisher =
     open Microsoft.FSharp.Collections
 
-    let private diffInitializer i j =
-      if i = 0 && j = 0 then 0
-      elif j = 0 then i
-      elif i = 0 then j
-      else 0
+    let private insertCost = 1
+    let private deleteCost = 1
+    let private substitiuteCost = 1
+
+    let diffInitializer i j =
+      match (i, j) with
+        | (0, 0) -> 0
+        | (0, value) -> value
+        | (value, 0) -> value
+        | (_, _) -> 0
+
+    let private calculateSubstituteCost (firstItem: 'T) (secondItem: 'T) =
+      if firstItem = secondItem then 0 else substitiuteCost
 
     let private initCell (firstArray: 'T[]) (secondArray: 'T[]) (diffMatrix: int[,]) i j _ =
       if i = 0 || j = 0 then ignore()
       else
-        let changeCost = if firstArray.[i-1] = secondArray.[j-1] then 0 else 1
-        // insertion; deletion; substitution
-        Array.min [|diffMatrix.[i-1, j] + 1; diffMatrix.[i, j-1] + 1; diffMatrix.[i-1, j-1] + changeCost|]
+        Array.min [|
+          diffMatrix.[i-1, j] + insertCost;
+          diffMatrix.[i, j-1] + deleteCost;
+          diffMatrix.[i-1, j-1] + calculateSubstituteCost firstArray.[i-1] secondArray.[j-1]
+        |]
         |> Array2D.set diffMatrix i j
 
 
