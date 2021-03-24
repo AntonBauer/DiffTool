842f222b458f95832bb823916adc7a6dd85d4cbe
BauerAV <abton.bauer@gmail.com>
2021-03-24 07:31:57 +0100

    Add vs code debug configuration

--- Algorithms.EditDistance/WagnerFisherAlgorithm.fs
+++ Algorithms.EditDistance/WagnerFisherAlgorithm.fs
@@ -9,19 +9,19 @@ namespace Algorithms.EditDistance
       elif i = 0 then j
       else 0
 
-    let private initCell (firstArray: array<'T>) (secondArray: array<'T>) (diffMatrix: int[,]) i j _ =
+    let private initCell (firstArray: 'T[]) (secondArray: 'T[]) (diffMatrix: int[,]) i j _ =
       if i = 0 || j = 0 then ignore()
       else
         let changeCost = if firstArray.[i-1] = secondArray.[j-1] then 0 else 1
-        // deletion; insertion; substitution
+        // insertion; deletion; substitution
         Array.min [|diffMatrix.[i-1, j] + 1; diffMatrix.[i, j-1] + 1; diffMatrix.[i-1, j-1] + changeCost|]
         |> Array2D.set diffMatrix i j
 
 
-    let buildDifferenceMatrix<'T when 'T : equality> (firstArray: array<'T>) (secondArray: array<'T>) =
-      let diffMatrix = Array2D.init (firstArray.Length + 1) (secondArray.Length + 1) diffInitializer
+    let buildDifferenceMatrix (firstArray: 'T[]) (secondArray: 'T[]) =
+      let diffMatrix = Array2D.init (Array.length firstArray + 1) (Array.length secondArray + 1) diffInitializer
       
-      diffMatrix
-      |> Array2D.iteri (initCell firstArray secondArray diffMatrix)
+      (initCell firstArray secondArray diffMatrix, diffMatrix)
+      ||> Array2D.iteri
       
       diffMatrix
