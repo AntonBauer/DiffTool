cd4cfcf0a145e895f7d535a3114afaf033510b58
Anton Bauer <abton.bauer@gmail.com>
2023-11-29 19:59:51 +0100

    renamings

--- DiffTool/Diff.fs
+++ DiffTool/Diff.fs
@@ -14,23 +14,23 @@ module Diff =
 
     let vagnerFisher: VagnerFisher<'a> =
         fun firstArray secondArray ->
-            let editingMatrix =
+            let diffMatrix =
                 Array2D.zeroCreate (firstArray.Length + 1) (secondArray.Length + 1)
 
             for i in 1 .. firstArray.Length + 1 do
                 for j in 1 .. secondArray.Length + 1 do
                     if (firstArray[i - 1] = secondArray[j - 1]) then
-                        Array2D.set editingMatrix i j editingMatrix[i - 1, j - 1]
+                        Array2D.set diffMatrix i j diffMatrix[i - 1, j - 1]
                     else
-                        let insertion = editingMatrix[i - 1, j]
-                        let deletion = editingMatrix[i, j - 1]
-                        let substitution = editingMatrix[i - 1, j - 1]
-                        Array2D.set editingMatrix i j (min insertion (min deletion substitution))
+                        let insertion = diffMatrix[i - 1, j]
+                        let deletion = diffMatrix[i, j - 1]
+                        let substitution = diffMatrix[i - 1, j - 1]
+                        Array2D.set diffMatrix i j (min insertion (min deletion substitution))
 
-            editingMatrix
+            diffMatrix
 
     let lee: Lee =
-        fun editingMatrix ->
+        fun diffMatrix ->
             let tst =
                 { Insertions = Array.empty
                   Deletions = Array.empty
